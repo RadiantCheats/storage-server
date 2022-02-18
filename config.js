21203c566705ec4f8d7a7cefad96ba5c39639276
Luck <luckplaysmcpe@gmail.com>
2022-02-17 23:14:47 -0500

    Config setup

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-    "keys": ['ohno'],
+    "keys": ['yCFuBfXIgB'],
     "port": 9000,
     "salt_key": "lu3kissofuckingc00lomg!heisn1ce2", // Must be 32 bytes or an error will occur
     "secret": "imanembarassmenttomyfamily"
